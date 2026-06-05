10e8d0b205e3918d9d7ddab154b30e866adec9d8
yahiro <yahiro07@users.noreply.github.com>
2026-06-05 17:54:12 +0900

    import entries from mofur

--- mofus/tsdown.config.ts
+++ mofus/tsdown.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: {
+    "ax/index": "../mofur/src/ax/index.ts",
+    "ax-ui/index": "../mofur/src/ax-ui/index.ts",
+    "mo/index": "../mofur/src/mo/index.ts",
+    "mo-audio/index": "../mofur/src/mo-audio/index.ts",
+    "mo-synthesis/index": "../mofur/src/mo-synthesis/index.ts",
+    "mx-audio/index": "../mofur/src/mx-audio/index.ts",
+    //
     "ax-solid/index": "src/ax-solid/index.ts",
     "mo-solid/index": "src/mo-solid/index.ts",
   },
