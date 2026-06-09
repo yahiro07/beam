48c8f883abd14146b29321dd62d8c5732d6734d1
yahiro <yahiro07@users.noreply.github.com>
2026-06-10 01:18:32 +0900

    add reverse option

--- mofur-components/src/mono2/general-selector.tsx
+++ mofur-components/src/mono2/general-selector.tsx
@@ -1,16 +1,26 @@
+import { useMemo } from "react";
 import { SelectorOption } from "../common/selector-option";
 
 type Props<T extends string | number> = {
   options: SelectorOption<T>[];
   value: T;
   onChange: (value: T) => void;
+  reverseOptionsOrder?: boolean;
 };
 
 export function GeneralSelector<T extends string | number>({
   options,
   value,
   onChange,
+  reverseOptionsOrder = false,
 }: Props<T>) {
+  const orderedOptions = useMemo(() => {
+    if (reverseOptionsOrder) {
+      return [...options].reverse();
+    }
+    return options;
+  }, [options, reverseOptionsOrder]);
+
   const wrapOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const isNumber = typeof options[0].value === "number";
     const newValue = isNumber ? parseFloat(e.target.value) : e.target.value;
@@ -22,7 +32,7 @@ export function GeneralSelector<T extends string | number>({
       onChange={wrapOnChange}
       // className="border border-gray-400 px-1 py-2 w-[400px]"
     >
-      {options.map((opt) => (
+      {orderedOptions.map((opt) => (
         <option key={opt.value} value={opt.value}>
           {opt.label}
         </option>
